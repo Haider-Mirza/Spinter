ddaddfff1ca5755f6483bffb7d9a7f4d9ca8e9bb
Haider Mirza <x7and7@gmail.com>
2022-04-02 13:35:12 +0100

    Added a README.org

--- project.sh
+++ project.sh
@@ -26,4 +26,5 @@ elif [ $((x)) == 3 ]; then
 elif [ $((x)) == 4 ]; then
     echo -e "\n"
     cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=1 -S . -B build/
+    ln -s build/compile_commands.json compile_commands.json 
 fi
