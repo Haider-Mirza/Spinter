f1758c96c65775a173648e73a70ec14eb7c95731
Haider Mirza <x7and7@gmail.com>
2022-04-15 22:32:22 +0100

    For some reason, all files got deleted from git repo

---
+++ manifest.scm
@@ -0,0 +1,18 @@
+(specifications->manifest
+ '(;; C++ Development
+
+   ;; CMake 
+   "cmake"
+   "make"
+   "gcc"
+   "gcc-toolchain"
+
+   ;; General Navigation
+   "binutils"
+
+   ;; Dependancies
+   "glfw"
+
+   ;; Font rendering
+   "freetype"
+   "fontconfig"))
