9dabdd007d617d299f5db40c84795c9e3098e725
Haider Mirza <x7and7@gmail.com>
2022-06-18 21:27:39 +0100

    new directory structure on my part.
    * compile_commands.json. new directory structure on my part. *Modified file(s)*

--- spinter.sh
+++ spinter.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Options
-echo -e "1) Compile and run project\n2) Build Project\n3) Run Project\n4) Configure CMake"
+echo -e "1) Compile and run project\n2) Build Project\n3) Run Project\n4) Configure CMake\n5) Copy compile_commands.json over to project root"
 echo -n "Enter Number: "
 read x
 
@@ -26,6 +26,7 @@ elif [ $((x)) == 3 ]; then
 elif [ $((x)) == 4 ]; then
     echo -e "\n"
     cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DGLFW_BUILD_DOCS=OFF -S . -B build/
-fi
 
-# Submodule
+elif [ $((x)) == 5 ]; then
+    ln -s build/compile_commands.json compile_commands.json
+fi
